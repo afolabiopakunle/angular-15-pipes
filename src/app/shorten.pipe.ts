b751cccca39ede9070196654c92be0dbb7777f88
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-27 14:05:35 +0100

    arguements added

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,7 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any, length?: any): any {
+    if(length > 0) {
+      return value.substr(0, length) + '...';
+    }
     return value.substr(0, 12) + '...';
   }
 
